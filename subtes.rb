f316129a1ff9a8807e099f08c3df05fc33929682
Julio Olivera <julio.olivera@threepillarglobal.com>
2012-12-05 11:10:14 -0300

    Fixed regex

--- subtes.rb
+++ subtes.rb
@@ -2,6 +2,6 @@ require 'net/http'
 
 def get_status()
   source = Net::HTTP.get(URI('http://www.metrovias.com.ar/V2/InfoSubteSplash.asp'))
-  regexp = /pausecontent\[\d+\]\s=\s.*?L*nea (\w).*?; (.*?)'/m
+  regexp = /pausecontent\[\d+\]\s*=.*?L.nea (\w).*?;(.*?)'/m
   Hash[source.scan(regexp)]
 end
